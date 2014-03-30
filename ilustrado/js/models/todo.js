9e89ff05801dcd0e51c2893bd75870b63d27a84c
walis85300 <walojose46@gmail.com>
2014-03-29 22:30:38 -0430

    Creacion de ejemplo

---
+++ ilustrado/js/models/todo.js
@@ -0,0 +1,18 @@
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+
+	defaults: {
+
+		title: '',
+		completed: false
+
+	},
+
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+
+});
--- propio/js/main.js
+++ propio/js/main.js
@@ -2,10 +2,13 @@ $(function(){
 
 	Backbone.$ = $;
 
-	var Todo = Backbone.Model.extend({});
+	var Todo = Backbone.Model.extend({
+		url: ''
+	});
 
 	var TodoList = Backbone.Collection.extend({
 
+		url: '/todos',
 		model: Todo
 
 	});
@@ -15,6 +18,13 @@ $(function(){
 	var TodoView = Backbone.View.extend({
 
 		tagName: 'li',
+
+		className: 'todo',
+
+		events: {
+			"dblclick .title" : "edit",
+			"keypress .edit" : "updateOnEnter"
+		},
 
 		template: Handlebars.compile($("#item-template").html()),
 
@@ -29,8 +39,39 @@ $(function(){
 			var item = 	this.model.toJSON();
 			var html = this.template(item);
 			this.$el.html(html);
+			this.input = this.$(".edit");
 			return this;
 
+		},
+
+		edit: function(){
+			this.$el.addClass("editing");
+			this.input.focus();
+		},
+
+		updateOnEnter: function(e){
+
+			if(e.keyCode == 13) this.close();
+
+		},
+
+		close: function(){
+
+			var value = this.input.val();
+
+			if(!value) {
+				this.clear();
+			} else {
+
+				this.$el.removeClass("editing");
+				this.model.save({title: value});
+
+			}
+
+		},
+
+		clear: function(){
+			this.model.destroy();
 		}
 
 	});
@@ -39,7 +80,7 @@ $(function(){
 
 		el: $('#todos'),
 
-		// template: Handlebars.compile($("#item-template").html()),
+		template: Handlebars.compile($("#item-template").html()),
 
 		events: {
 
